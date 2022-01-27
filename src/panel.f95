1003f6ee9c102ce464026679d638928e0f188cdb
Cory <cory.goates@aggiemail.usu.edu>
2022-01-27 11:42:29 -0700

    Added passing of DoD info to Johnson integral functions.

--- src/panel.f95
+++ src/panel.f95
@@ -901,7 +901,7 @@ contains
     end function panel_get_field_point_geometry
 
 
-    function panel_E_i_M_N_K(this, geom, i, M, N, K) result(E)
+    function panel_E_i_M_N_K(this, geom, i, M, N, K, dod_info) result(E)
         ! Calculates E_i(M,N,K)
 
         implicit none
@@ -909,8 +909,11 @@ contains
         class(panel),intent(in) :: this
         type(eval_point_geom),intent(in) :: geom
         integer,intent(in) :: i, M, N, K
+        type(dod),intent(in) :: dod_info
 
-        real :: E, E_1, E_2
+        real :: E
+
+        real :: E_1, E_2
 
         ! Evaluate at start vertex
         E_1 = ((this%vertices_ls(i,1)-geom%r_in_plane(1))**(M-1) &
@@ -928,7 +931,7 @@ contains
     end function panel_E_i_M_N_K
 
 
-    function panel_F_i_1_1_1(this, geom, i) result(F)
+    function panel_F_i_1_1_1(this, geom, i, dod_info) result(F)
         ! Calculates F_i(1,1,1)
 
         implicit none
@@ -936,6 +939,8 @@ contains
         class(panel) :: this
         type(eval_point_geom),intent(in) :: geom
         integer,intent(in) :: i
+        type(dod),intent(in) :: dod_info
+
         real :: F
 
         ! Calculate F(1,1,1)
@@ -955,7 +960,7 @@ contains
     end function panel_F_i_1_1_1
 
 
-    function panel_calc_F_integrals(this, geom, proc_H, MXK, MXQ, NHK) result (F)
+    function panel_calc_F_integrals(this, geom, proc_H, MXK, MXQ, NHK, dod_info) result (F)
         ! Calculates the F integrals necessary
 
         implicit none
@@ -963,6 +968,7 @@ contains
         class(panel),intent(in) :: this
         type(eval_point_geom),intent(in) :: geom
         integer,intent(in) :: proc_H, MXK, MXQ, NHK
+        type(dod),intent(in) :: dod_info
         real,dimension(:,:,:,:),allocatable :: F
 
         real :: E1, E2, v_xi, v_eta, dF
@@ -1009,7 +1015,7 @@ contains
             v_eta = this%n_hat_l(i,2)
 
             ! Calculate F(1,1,1)
-            F(i,1,1,1) = this%F_i_1_1_1(geom, i)
+            F(i,1,1,1) = this%F_i_1_1_1(geom, i, dod_info)
 
             ! Procedure 4: not close to perimeter
             if (geom%g(i) >= 0.01*dF) then
@@ -1018,8 +1024,8 @@ contains
                 do k=3,MXFK,2
 
                     ! Get necessary E
-                    E1 = this%E_i_M_N_K(geom, i, 2, 1, k-2)
-                    E2 = this%E_i_M_N_K(geom, i, 1, 2, k-2)
+                    E1 = this%E_i_M_N_K(geom, i, 2, 1, k-2, dod_info)
+                    E2 = this%E_i_M_N_K(geom, i, 1, 2, k-2, dod_info)
 
                     ! Calculate F
                     F(i,1,1,k) = 1./(geom%g2(i)*(k-2))*((k-3)*F(i,1,1,k-2)-v_eta*E1+v_xi*E2)
@@ -1035,8 +1041,8 @@ contains
                 do k=MXFK+NFK,5,2
 
                     ! Get necessary E
-                    E1 = this%E_i_M_N_K(geom, i, 2, 1, k-2)
-                    E2 = this%E_i_M_N_K(geom, i, 1, 2, k-2)
+                    E1 = this%E_i_M_N_K(geom, i, 2, 1, k-2, dod_info)
+                    E2 = this%E_i_M_N_K(geom, i, 1, 2, k-2, dod_info)
 
                     ! Calculate F
                     F(i,1,1,k-2) = 1./(k-3)*(geom%g2(i)*(k-2)*F(i,1,1,k)+v_eta*E1-v_xi*E2)
@@ -1051,7 +1057,7 @@ contains
                 do n=2,MXQ
 
                     ! Get E
-                    E1 = this%E_i_M_N_K(geom, i, 1, n-1, -1)
+                    E1 = this%E_i_M_N_K(geom, i, 1, n-1, -1, dod_info)
 
                     if (n .eq. 2) then
                         F(i,1,N,1) = 1./(n-1)*((2*n-3)*geom%a(i)*v_eta*F(i,1,n-1,1) + v_xi*E1)
@@ -1075,7 +1081,7 @@ contains
                 do m=2,MXQ
 
                     ! Get E
-                    E1 = this%E_i_M_N_K(geom, i, m-1, 1, -1)
+                    E1 = this%E_i_M_N_K(geom, i, m-1, 1, -1, dod_info)
 
                     if (m .eq. 2) then
                         F(i,m,1,1) = 1./(m-1)*((2*m-3)*geom%a(i)*v_xi*F(i,m-1,1,1) - v_eta*E1)
@@ -1097,7 +1103,7 @@ contains
 
             ! Calculate F(1,2,K) integrals
             do k=3,MXK-2,2
-                F(i,1,2,k) = v_eta*geom%a(i)*F(i,1,1,k)-v_xi/(k-2)*this%E_i_M_N_K(geom, i, 1, 1, k-2)
+                F(i,1,2,k) = v_eta*geom%a(i)*F(i,1,1,k)-v_xi/(k-2)*this%E_i_M_N_K(geom, i, 1, 1, k-2, dod_info)
             end do
 
             ! Calculate F(1,N,K) integrals
@@ -1117,7 +1123,7 @@ contains
     end function panel_calc_F_integrals
 
 
-    function panel_calc_H_integrals(this, geom, proc_H, MXK, MXQ, NHK, F) result(H)
+    function panel_calc_H_integrals(this, geom, proc_H, MXK, MXQ, NHK, dod_info, F) result(H)
         ! Calculates the necessary H integrals
 
         implicit none
@@ -1125,6 +1131,7 @@ contains
         class(panel),intent(in) :: this
         type(eval_point_geom),intent(in) :: geom
         integer,intent(in) :: proc_H, MXK, MXQ, NHK
+        type(dod),intent(in) :: dod_info
         real,dimension(:,:,:,:),allocatable,intent(in) :: F
         real,dimension(:,:,:),allocatable :: H
 
@@ -1304,7 +1311,7 @@ contains
     end function nu_M_N_K
 
 
-    subroutine panel_calc_integrals(this, geom, influence_type, singularity_type, H, F)
+    subroutine panel_calc_integrals(this, geom, influence_type, singularity_type, dod_info, H, F)
         ! Calculates the H and F integrals necessary for the given influence
 
         implicit none
@@ -1312,6 +1319,7 @@ contains
         class(panel),intent(in) :: this
         type(eval_point_geom),intent(in) :: geom
         character(len=*),intent(in) :: influence_type, singularity_type
+        type(dod),intent(in) :: dod_info
         real,dimension(:,:,:,:),allocatable,intent(out) :: F
         real,dimension(:,:,:),allocatable,intent(out) :: H
 
@@ -1382,10 +1390,10 @@ contains
         end if
 
         ! Calculate F integrals
-        F = this%calc_F_integrals(geom, proc_H, MXK, MXQ, NHK)
+        F = this%calc_F_integrals(geom, proc_H, MXK, MXQ, NHK, dod_info)
 
         ! Calculate H integrals
-        H = this%calc_H_integrals(geom, proc_H, MXK, MXQ, NHK, F)
+        H = this%calc_H_integrals(geom, proc_H, MXK, MXQ, NHK, dod_info, F)
 
     end subroutine panel_calc_integrals
 
@@ -1882,7 +1890,7 @@ contains
             if (influence_calc_type == 'johnson') then
 
                 ! Get integrals
-                call this%calc_integrals(geom, "potential", "source", H, F)
+                call this%calc_integrals(geom, "potential", "source", dod_info, H, F)
 
                 if (source_order == 0) then
 
@@ -1943,7 +1951,7 @@ contains
             if (influence_calc_type == 'johnson') then
 
                 ! Get integrals
-                call this%calc_integrals(geom, "velocity", "source", H, F)
+                call this%calc_integrals(geom, "velocity", "source", dod_info, H, F)
 
                 if (source_order == 0) then
 
@@ -2022,7 +2030,7 @@ contains
             if (influence_calc_type == 'johnson') then
 
                 ! Get integrals
-                call this%calc_integrals(geom, "potential", "doublet", H, F)
+                call this%calc_integrals(geom, "potential", "doublet", dod_info, H, F)
 
                 ! Calculate influence
                 if (doublet_order == 1) then
@@ -2127,7 +2135,7 @@ contains
             if (influence_calc_type == 'johnson') then
 
                 ! Get integrals
-                call this%calc_integrals(geom, "velocity", "doublet", H, F)
+                call this%calc_integrals(geom, "velocity", "doublet", dod_info, H, F)
 
                 if (doublet_order .eq. 1) then
 
