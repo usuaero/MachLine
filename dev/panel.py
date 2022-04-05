b7948ce221c4750a43dbe8b921311bf48681f9d5
Cory <cory.goates@aggiemail.usu.edu>
2022-04-05 15:47:03 -0600

    Added calculation of Q1 for subsonic edges.

--- dev/panel.py
+++ dev/panel.py
@@ -6,13 +6,22 @@ class Panel:
     Parameters
     ----------
     verts : ndarray
-        2x3 array, the columns of which are the corner points of the panel, expressed in local coords.
+        2xN array, the columns of which are the corner points of the panel, expressed in local coords.
+
+    mu : float
+        Doublet strength.
+
+    sigma : float
+        Source strength.
     """
 
-    def __init__(self, verts):
+    def __init__(self, verts, mu, sigma):
 
         # Store
         self.verts = verts
+        self.N = self.verts.shape[1]
+        self.mu = mu
+        self.sigma = sigma
 
         # Calculate edge slopes
         self.m = (np.roll(verts[1,:], 1)-verts[1,:])/(np.roll(verts[0,:], 1)-verts[0,:])
@@ -21,10 +30,120 @@ class Panel:
         # Calculate area
         d0 = self.verts[:,2] - self.verts[:,1]
         d1 = self.verts[:,1] - self.verts[:,0]
-        self.A = 0.5*np.abs(d0[0]*d1[1] - d0[1]*d1[0])
+        if self.N == 3:
+            self.A = 0.5*abs(d0[0]*d1[1] - d0[1]*d1[0])
+        else:
+            self.A = abs(d0[0]*d1[1] - d0[1]*d1[0])
 
         # Calculate centroid
-        self.c = np.sum(self.verts, axis=1)/3.0
+        self.c = np.sum(self.verts, axis=1)/self.N
+
+
+    def _calc_geometry(self, P):
+        # Calculates necessary geometry
+
+        # Set up geometry
+        h = P[2]
+        xm = np.zeros(self.N)
+        ym1 = np.zeros(self.N)
+        ym2 = np.zeros(self.N)
+        R1 = np.zeros(self.N)
+        R2 = np.zeros(self.N)
+
+        # Loop through edges
+        for i in range(self.N):
+            i_next = (i+1)%self.N
+
+            # Get displacements
+            sm1 = P[0] - self.verts[0,i]
+            s1 = P[1] - self.verts[1,i]
+            sm2 = P[0] - self.verts[0,i_next]
+            s2 = P[1] - self.verts[1,i_next]
+
+            # Get hyperbolic radii
+            x = sm1**2 - s1**2 - h**2
+            if x > 0.0:
+                R1[i] = np.sqrt(x)
+            else:
+                R1[i] = 0.0
+
+            x = sm2**2 - s2**2 - h**2
+            if x > 0.0:
+                R2[i] = np.sqrt(x)
+            else:
+                R2[i] = 0.0
+
+            # Calculate edge-based coordinates
+
+            # Subinclined edge
+            if abs(self.m[i]) <= 1.0:
+
+                xm[i] = s1 - sm1*self.m[i]
+                ym1[i] = sm1 - s1*self.m[i]
+                ym2[i] = sm2 - s2*self.m[i]
+
+            # Superinclined edge
+            else:
+
+                xm[i] = sm1 - s1*self.l[i]
+                ym1[i] = s1 - sm1*self.l[i]
+                ym2[i] = s2 - sm2*self.l[i]
+
+        return h, xm, ym1, ym2, R1, R2
+
+
+    def _calc_integrals(self, P):
+        # Calculates the needed integrals
+
+        # Calculate geometry
+        h, xm, ym1, ym2, R1, R2 = self._calc_geometry(P)
+
+        # Initialize
+        Q1 = 0.0
+        w0 = np.zeros(self.N)
+
+        # Loop through edges
+        for i in range(self.N):
+
+            # Calculate Q1
+
+            # Subsonic or sonic edge
+            if abs(self.m[i]) <= 1.0:
+
+                # Both endpoints in
+                if R1[i] != 0.0 and R2[i] != 0.0:
+
+                    A = h*xm[i]*(ym1[i]*R2[i] - ym2[i]*R1[i])
+                    B = h**2*ym1[i]*ym2[i] + xm[i]**2*R1[i]*R2[i]
+
+                    Q1 += np.arctan2(A, B)
+
+                # First endpoint in
+                elif R1[i] != 0.0:
+
+                    Q1 += -np.sign(h)*np.arctan2(xm[i]*R1[i], abs(h)*ym1[i])
+
+                # Second endpoint in
+                elif R1[i] != 0.0:
+
+                    Q1 += np.sign(h)*np.arctan2(xm[i]*R2[i], abs(h)*ym2[i])
+
+            # Calculate w0
+            
+            # Subsonic edge
+            if abs(self.m[i]) < 1.0:
+
+                x = np.sqrt(1.0-self.m[i]**2)
+
+                # Both endpoints in
+                if R1[i] != 0.0 and R2[i] != 0.0:
+
+                    A = ym2[i] + x*R2[i]
+                    B = ym1[i] + x*R1[i]
+
+                    w0[i] = np.arctan2(A, B)
+
+        return Q1, w0
 
 
     def calc_induced_source_potential(self, P):
@@ -35,6 +154,12 @@ class Panel:
         P : ndarray
             Evaluation point.
         """
+
+        # Calculate geometry
+        h, xm, ym1, ym2, R1, R2 = self._calc_geometry(P)
+
+        # Calculate integrals
+        Q1, w0 = self._calc_integrals(P)
 
         phi_s = 0.0
         return phi_s
@@ -49,5 +174,12 @@ class Panel:
             Evaluation point.
         """
 
-        phi_d = 0.0
+        # Calculate geometry
+        h, xm, ym1, ym2, R1, R2 = self._calc_geometry(P)
+
+        # Calculate integrals
+        Q1, w0 = self._calc_integrals(P)
+
+        phi_d = -0.5*self.mu*Q1/np.pi
+
         return phi_d
--- dev/test_panel.py
+++ dev/test_panel.py
@@ -8,17 +8,17 @@ from singularities import Source, Doublet
 if __name__=="__main__":
 
     # Initialize panel
-    verts = np.array([[0.0, 1.0, 0.0],
-                      [0.0, 0.0, 1.0]])
-    p = Panel(verts)
+    verts = np.array([[0.0, 1.0, 2.0, 1.0],
+                      [0.0, -0.5, 0.0, 0.5]])
+    p = Panel(verts, 1.0, 1.0)
 
     # Initialize equivalent point singularities
     s = Source(np.array([p.c[0], p.c[1], 0.0]), p.A)
     d = Doublet(np.array([p.c[0], p.c[1], 0.0]), p.A)
 
     # Initialize ray of evaluation points
-    P0 = np.array([0.0, 0.0, 0.0])
-    d0 = np.array([1.0, 0.0, -0.0])
+    P0 = np.array([5.0, 0.0, -5.0])
+    d0 = np.array([0.0, 0.0, 1.0])
     d0 /= np.linalg.norm(d0)
     k = np.linspace(0.0, 10.0, 500)
     phi_s_point = np.zeros_like(k)
@@ -40,19 +40,18 @@ if __name__=="__main__":
 
     # Plot
     fig, ax = plt.subplots(ncols=2, figsize=(12.0, 7.0))
+    plt.suptitle("Panel with purely subsonic edges.")
 
     # Source influences
-    ax[0].plot(k, phi_s_point, 'k-', label='Point')
-    ax[0].plot(k, phi_s_panel, 'k--', label='Panel')
-    ax[0].set_title('Source-Induced Potential')
+    ax[0].plot(k, phi_s_point, 'k--', label='Point')
+    ax[0].plot(k, phi_s_panel, 'k-', label='Panel')
     ax[0].set_xlabel('Distance')
     ax[0].set_ylabel('$\phi_s$')
     ax[0].legend()
 
     # Doublet influences
-    ax[1].plot(k, phi_d_point, 'k-', label='Point')
-    ax[1].plot(k, phi_d_panel, 'k--', label='Panel')
-    ax[1].set_title('Doublet-Induced Potential')
+    ax[1].plot(k, phi_d_point, 'k--', label='Point')
+    ax[1].plot(k, phi_d_panel, 'k-', label='Panel')
     ax[1].set_xlabel('Distance')
     ax[1].set_ylabel('$\phi_d$')
     ax[1].legend()
