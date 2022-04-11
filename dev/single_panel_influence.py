53c3480eb52101aff1fa52523962b513db6a897b
Cory <cory.goates@aggiemail.usu.edu>
2022-04-11 11:37:29 -0600

    Improved structure of things and added more tests.

--- dev/single_panel_influence.py
+++ dev/single_panel_influence.py
@@ -5,11 +5,9 @@ from panel import Panel
 from singularities import Source, Doublet
 
 
-if __name__=="__main__":
+def plot_comparison(verts, P0, d0, figname):
 
     # Initialize panel
-    verts = np.array([[0.0, 1.0, 2.0, 1.0],
-                      [0.0, -1.5, 0.0, 1.5]])
     p = Panel(verts, 1.0, 1.0)
 
     # Initialize equivalent point singularities
@@ -17,8 +15,6 @@ if __name__=="__main__":
     d = Doublet(np.array([p.c[0], p.c[1], 0.0]), p.A)
 
     # Initialize ray of evaluation points
-    P0 = np.array([10.0, 0.0, -5.0])
-    d0 = np.array([0.0, 0.0, 1.0])
     d0 /= np.linalg.norm(d0)
     k = np.linspace(0.0, 10.0, 500)
     phi_s_point = np.zeros_like(k)
@@ -40,7 +36,6 @@ if __name__=="__main__":
 
     # Plot
     fig, ax = plt.subplots(ncols=2, figsize=(12.0, 7.0))
-    plt.suptitle("Panel with purely subsonic edges.")
 
     # Source influences
     ax[0].plot(k, phi_s_point, 'k--', label='Point')
@@ -56,4 +51,31 @@ if __name__=="__main__":
     ax[1].set_ylabel('$\phi_d$')
     ax[1].legend()
 
-    plt.show()
+    plt.savefig(figname)
+
+
+if __name__=="__main__":
+
+    verts = np.array([[0.0, 1.0, 2.0, 1.0],
+                      [0.0, -0.5, 0.0, 0.5]])
+    P0 = np.array([10.0, 0.0, -5.0])
+    d0 = np.array([0.0, 0.0, 1.0])
+    plot_comparison(verts, P0, d0, 'dev/unit_tests/subsonic_edges_z_trans_totally_in.pdf')
+
+    verts = np.array([[0.0, 1.0, 2.0, 1.0],
+                      [0.0, -1.5, 0.0, 1.5]])
+    P0 = np.array([10.0, 0.0, -5.0])
+    d0 = np.array([0.0, 0.0, 1.0])
+    plot_comparison(verts, P0, d0, 'dev/unit_tests/supersonic_edges_z_trans_totally_in.pdf')
+
+    verts = np.array([[0.0, 1.0, 2.0, 1.0],
+                      [0.0, -0.5, 0.0, 0.5]])
+    P0 = np.array([3.5, 0.0, 1.0])
+    d0 = np.array([1.0, 0.0, 0.0])
+    plot_comparison(verts, P0, d0, 'dev/unit_tests/subsonic_edges_x_trans_totally_in.pdf')
+
+    verts = np.array([[0.0, 1.0, 2.0, 1.0],
+                      [0.0, -1.5, 0.0, 1.5]])
+    P0 = np.array([3.5, 0.0, 1.0])
+    d0 = np.array([1.0, 0.0, 0.0])
+    plot_comparison(verts, P0, d0, 'dev/unit_tests/supersonic_edges_x_trans_totally_in.pdf')
