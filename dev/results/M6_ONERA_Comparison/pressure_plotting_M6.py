7378bd042393b28b74cc90e108810c77b3e077db
Ammon Houser <A02077774@usu.edu>
2022-05-11 14:03:28 -0600

    Updated M6 Onera presure plotting and paraview data processing to handle different pressure calculations and plot them against one another.

--- dev/results/M6_ONERA_Comparison/pressure_plotting_M6.py
+++ dev/results/M6_ONERA_Comparison/pressure_plotting_M6.py
@@ -9,59 +9,73 @@ from sklearn.metrics import precision_recall_curve
 
 class Swept_Plotting:
 
-    def __init__(self, Geometry_Data, data_type, y_locations):
+    def __init__(self, Geometry_Data, data_type, y_locations, process_points, calculation_type):
 
         self.locations = Geometry_Data
-
         self.semispan_y_loc = y_locations
-
         self.data_type = data_type
-       
+        self.datapoints = process_points
+        self.calc_type = calculation_type
 
-    def Pressure_Plot(self, file_name, data_to_plot):
+    def Pressure_Plot(self, file_name, datatype):
 
+        
+        # Create a copy of the process points and reformat for the legend
+        temp = np.copy(self.datapoints)
+        temp = np.char.replace(temp, "_", "",1)
+        new_temp = []
+        for name in temp:
+            for j, letter in enumerate(name):
+                if letter == "_":
+                    spot = j
+            new_name = name[:spot+1] + "{" + name[spot+1:] + "}"
+            new_temp.append(new_name)
+
+        # Create a dictionary to contain the legend names properly formatted for the requested parameters
+        labels_dict = dict(zip(self.datapoints, new_temp))
+
+        
         Data_complete = np.genfromtxt(file_name, delimiter=",", skip_header=0, dtype=str)
+        # Reformate Data_complete to remove multiple quotes
+        Data_complete = np.char.replace(Data_complete,'"',"")
+
         Data = np.genfromtxt(file_name, delimiter=",", skip_header=1, dtype=float)
 
-        # Iterate over file columns searching for the x and Cp columns
-        for i, val in enumerate(Data_complete[0,:]):
-            if "C_p" in val or "Cp" in val:
-                y_loc = i
-            if 'Points:0' in val:
-                x_loc = i
+        # Create lists to pull from for plotting differentiation
+        opacity = [1, 0.5, 0.35, 0.2, 0.1]
+        # shape = [".", "x", "s", "p", "D"]
+        # colors = ["g", "b", "r", "m", "y"]
 
-        C_p_inc = Data[:,y_loc]
-        x = Data[:,x_loc]
+        # Iterate over points to process
+        for j, point in enumerate(self.datapoints):
+            # Verify requested point is in the dataset
+            if point not in Data_complete[0,:]:
+                print(f'***{point} is not found in {file_name}. Quitting')
+                exit()
 
-        # Calculate local chord length and LE location for each section analyzed
-        Chord_Length = max(x) - min(x)
-        LE_loc = min(x)
-
-        # Ensure the plots are colored consistently
-
-        if data_to_plot == 'MachLine':
-            shape = "o"
-            opacity = 1
-            colors = 'g'
-
-        elif data_to_plot == 'FlightStream':
-            shape = "x"
-            opacity = 0.5
-            colors = 'b'
-
-        else:
-            shape = "4"   
-            opacity = 0.1
-            colors = 'y'
-        
-        # Black and white lines with varying opacity
-        plt.plot((x-LE_loc)/Chord_Length,C_p_inc, label=data_to_plot, alpha=opacity, color="k")
-        
-        # Various colors
-        # plt.plot((x-LE_xy_loc)/Chord_Length,C_p_inc, "o", label=data_to_plot, color=colors)
-        
-        # Markers alone without lines for all data
-        # plt.plot((x-LE_loc)/Chord_Length,C_p_inc, label=data_to_plot, marker=shape, color="k", linestyle="none")
+            # Iterate over file columns searching for the x and Cp columns
+            for i, val in enumerate(Data_complete[0,:]):
+                if val == 'Points:0':
+                    x_loc = i
+                if val == point:
+                    point_loc = i
+                
+            point_data = Data[:,point_loc]
+            x = Data[:,x_loc]
+            
+            # Calculate local chord length and LE location for each section analyzed
+            Chord_Length = max(x) - min(x)
+            LE_loc = min(x)
+            x_axis = (x-LE_loc)/Chord_Length
+            
+            # Black and white lines with varying opacity
+            plt.plot(x_axis, point_data, label=f"${labels_dict[point]}$", alpha=opacity[j], color="k")
+            
+            # Various colors
+            # plt.plot((x-LE_xy_loc)/Chord_Length,point_data, "o", label=datatype, color=colors[j])
+            
+            # Markers alone without lines for all data
+            # plt.plot((x-LE_loc)/Chord_Length,point_data, label=datatype, marker=shape[j], color="k", linestyle="none")
 
         return
 
@@ -95,20 +109,28 @@ class Swept_Plotting:
 
             # Establish clean list for saving and combining produced plots
             list_of_files =[]
+                
             # Iterate over angles of attack for each semispan
             for j,AoA in enumerate(self.locations[percent_semispan]):
-               
                 # Pull in experimental results
                 file_experimental = f"Experimental_Data/M6_Data_mach_{mach_number[j]}_AoA_{AoA}.csv"
             
                 Experimental = np.genfromtxt(file_experimental, delimiter=",", skip_header=2, dtype=float, usecols=(section*2, section*2+1))
                 # Iterate over data sources
                 for data_type in self.data_type:
+                    if data_type == "MachLine":
 
-                    file = f"{data_type}_Data/M6_{data_type}_data_{percent_semispan}_percent_semispan_mach_{mach_number[j]}_AoA_{AoA}.csv"
+                        # Iterate over calculation type
+                        for calc in self.calc_type:
+                            file = f"{data_type}_Data/{calc}/M6_{data_type}_data_{percent_semispan}_percent_semispan_mach_{mach_number[j]}_AoA_{AoA}.csv"
+                            
+                            # Call plotting function
+                            self.Pressure_Plot(file, data_type)
+                    else:
+                        file = f"{data_type}_Data/M6_{data_type}_data_{percent_semispan}_percent_semispan_mach_{mach_number[j]}_AoA_{AoA}.csv"
                     
-                    # Call plotting function
-                    self.Pressure_Plot(file, data_type)
+                        # Call plotting function
+                        self.Pressure_Plot(file, data_type)
                     
                 # Invert the Y-axis
                 plt.gca().invert_yaxis()
@@ -166,29 +188,30 @@ inputfile = "M6_Onera_input_settings.json"
 json_string = open(inputfile).read()
 json_vals = json.loads(json_string)
 
-# Identify values to pass from input file
-
+# Identify geometry and flow conditions
 locations = json_vals["geometry"]["semispan locations and AoA"]
 semispan_y_loc = json_vals["geometry"]["semispan_y_loc"]
-
 datasets = json_vals["geometry"]["datasets"]
 mach_numbers = json_vals["flow conditions"]["mach_numbers"]
-formulation = json_vals["solver"]["formulation"]
 
+# Identify solver and plotting information
+formulation = json_vals["solver"]["formulation"]
+subsonic_calc_type = json_vals["solver"]["calculation_type"]
+points_to_process = json_vals["plots"]["points_to_process"]
 
 # Iterate over solver formulations from input file
 
     # Proccesses either one or all semispan locations based on input file
 if json_vals["plots"]["process_all"]:
 
-    Swept_Plotting(locations, datasets, semispan_y_loc).get_data(mach_numbers)
+    Swept_Plotting(locations, datasets, semispan_y_loc, points_to_process, subsonic_calc_type).get_data(mach_numbers)
 
 else:
     x = input("Enter percent semispan for analysis results. Options are 20, 44, 65, 80, 90, 95, 99:   ",)
     if x in locations.keys():
         Specific_Semispan = {x: locations[x]}
 
-        Swept_Plotting(Specific_Semispan, datasets, semispan_y_loc).get_data(mach_numbers)
+        Swept_Plotting(Specific_Semispan, datasets, semispan_y_loc, points_to_process, subsonic_calc_type).get_data(mach_numbers)
 
     else:
         print("\n****************\nInvalid Entry. Please run script again.\n****************\n")
