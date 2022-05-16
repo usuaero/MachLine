Guessed on doublet influence calculation. Everything is wrong, but we've got results.

dev/unit_tests/line_comparison.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from panel import SubinclinedPanel, SuperinclinedPanel
-from singularities import Source, Doublet
+from singularities import Source, Doublet, XDoublet
 
 
 def plot_comparison(verts, P0, d0, figname=None, subinclined=True):
@@ -9,12 +9,13 @@ def plot_comparison(verts, P0, d0, figname=None, subinclined=True):
     # Initialize panel
     if subinclined:
         p = SubinclinedPanel(verts, 1.0, 1.0)
+        d = Doublet(np.array([p.c[0], p.c[1], 0.0]), p.A)
     else:
         p = SuperinclinedPanel(verts, 1.0, 1.0)
+        d = XDoublet(np.array([p.c[0], p.c[1], 0.0]), p.A)
 
     # Initialize equivalent point singularities
     s = Source(np.array([p.c[0], p.c[1], 0.0]), p.A)
-    d = Doublet(np.array([p.c[0], p.c[1], 0.0]), p.A)
 
     # Initialize ray of evaluation points
     N_line = 500

dev/unit_tests/panel.py
@@ -468,7 +468,7 @@ class SuperinclinedPanel:
         # Get integrals
         psi, phi = self._calc_integrals(P)
 
-        return 0.0, psi, phi, a
+        return -self.sigma*psi/(2.0*np.pi), psi, phi, a
 
 
     def calc_induced_doublet_potential(self, P):
@@ -480,7 +480,10 @@ class SuperinclinedPanel:
             Evaluation point.
         """
 
+        # Get geometry
+        h,a,_,_,_,_,_ = self._calc_geometry(P)
+
         # Get integrals
         psi, phi = self._calc_integrals(P)
         
-        return 0.0
+        return self.mu*(-h**2*psi - np.sum(a*phi))/(2.0*np.pi)