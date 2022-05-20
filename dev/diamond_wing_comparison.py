e771241fd76da5f60b8ffcb5cbea3364fbe20cb2
Cory <cory.goates@aggiemail.usu.edu>
2022-05-20 10:57:39 -0600

    Added run results for diamond wing convergence study and shock-expansion comparison.

--- dev/diamond_wing_comparison.py
+++ dev/diamond_wing_comparison.py
@@ -6,66 +6,68 @@ import paraview.simple as pvs
 import matplotlib.pyplot as plt
 
 
-if __name__=="__main__":
+def run_comparison(M, alpha, grid, half_angle, run_machline=True):
+    # Runs the comparison of the diamond wing to shock-expansion theory
 
     # Parameters
-    M = 2.0
     R_G = 287.058
     gamma = 1.4
     T_inf = 300.0
     c_inf = np.sqrt(gamma*R_G*T_inf)
-    alpha = 0.0
     rho = 1.225
     p_inf = 1.0e5
-    grid = "medium" # coarse, medium, fine, ultra_fine
 
-    # Declare MachLine input
-    body_file = "dev/results/diamond_5_deg_full_{0}.vtk".format(grid)
-    input_dict = {
-        "flow": {
-            "freestream_velocity": [M*c_inf*np.cos(np.radians(alpha)), 0.0, M*c_inf*np.sin(np.radians(alpha))],
-            "gamma" : gamma,
-            "freestream_mach_number" : M
-        },
-        "geometry": {
-            "file": "dev/meshes/diamond_5_deg_full_{0}.stl".format(grid),
-            "spanwise_axis" : "+y",
-            "wake_model": {
-                "append_wake" : False,
+    # Storage locations
+    case_name = "M_{0}_aoa_{1}_{2}_deg_{3}".format(M, alpha, int(half_angle), grid)
+    plot_dir = "dev/results/diamond_wing_comparison/plots/"
+    body_file = "dev/results/diamond_wing_comparison/meshes/"+case_name+".vtk"
+    report_file = "dev/results/diamond_wing_comparison/reports/"+case_name+".json"
+    data_file = 'dev/results/diamond_wing_comparison/data/'+case_name+'.csv'
+
+    if run_machline:
+
+        # Declare MachLine input
+        input_dict = {
+            "flow": {
+                "freestream_velocity": [M*c_inf*np.cos(np.radians(alpha)), 0.0, M*c_inf*np.sin(np.radians(alpha))],
+                "gamma" : gamma,
+                "freestream_mach_number" : M
             },
-            "singularity_order" : {
-                "source" : 1
+            "geometry": {
+                "file": "dev/meshes/diamond_{0}_deg_full_{1}.stl".format(int(half_angle), grid),
+                "spanwise_axis" : "+y",
+                "wake_model": {
+                    "append_wake" : False,
+                },
+                "reference": {
+                    "area": 4.0
+                }
             },
-            "reference": {
-                "area": 4.0
+            "solver": {
+                "formulation": "morino",
+                "control_point_offset": 1.1e-8
+            },
+            "post_processing" : {
+                "pressure_rules" : {
+                    "second-order" : True,
+                    "isentropic" : True,
+                    "slender-body" : True,
+                    "linear" : True
+                }
+            },
+            "output" : {
+                "body_file" : body_file,
+                "report_file" : report_file
             }
-        },
-        "solver": {
-            "formulation": "morino",
-            #"formulation": "source-free",
-            "control_point_offset": 1.1e-8
-        },
-        "post_processing" : {
-            "pressure_rules" : {
-                "second-order" : True,
-                "isentropic" : True,
-                "slender-body" : True,
-                "linear" : True
-            }
-        },
-        "output" : {
-            "body_file" : body_file,
-            "control_point_file" : "dev/results/diamond_5_deg_full_{0}_control_points.vtk".format(grid)
         }
-    }
 
-    # Dump
-    input_file = "dev/diamond_input.json"
-    with open(input_file, 'w') as input_handle:
-        json.dump(input_dict, input_handle, indent=4)
+        # Dump
+        input_file = "dev/results/diamond_wing_comparison/diamond_input.json"
+        with open(input_file, 'w') as input_handle:
+            json.dump(input_dict, input_handle, indent=4)
 
-    # Run
-    sp.run(["./machline.exe", input_file])
+        # Run
+        sp.run(["./machline.exe", input_file])
     
     # Run shock-expansion comparison
     airfoil = fl.DiamondAirfoil(5.0, 1.0)
@@ -76,22 +78,19 @@ if __name__=="__main__":
     Cp3 = (p3/p_inf-1.0)/x
     Cp4 = (p4/p_inf-1.0)/x
     Cp5 = (p5/p_inf-1.0)/x
-    CL, CD = airfoil.get_inviscid_lift_and_drag_coefs()
-    Cx = -CL*np.sin(np.radians(alpha)) + CD*np.cos(np.radians(alpha))
-    Cz = CL*np.cos(np.radians(alpha)) + CD*np.sin(np.radians(alpha))
+    CL_se, CD_se = airfoil.get_inviscid_lift_and_drag_coefs()
+    
+    # Load MachLine report file
+    with open(report_file, 'r') as report_handle:
+        report = json.load(report_handle)
 
-    # Print table
-    print("{0:<20}{1:<20}".format("Surface", "Cp"))
-    print("".join(["-"]*40))
-    print("{0:<20}{1:<20}".format(2, Cp2))
-    print("{0:<20}{1:<20}".format(3, Cp3))
-    print("{0:<20}{1:<20}".format(4, Cp4))
-    print("{0:<20}{1:<20}".format(5, Cp5))
-    print()
-    print("CL:", CL)
-    print("CD:", CD)
-    print("Cx:", Cx)
-    print("Cz:", Cz)
+    # Extract lift and drag coefficients
+    Cx = report["total_forces"]["Cx"]
+    Cz = report["total_forces"]["Cz"]
+    C_a = np.cos(np.radians(alpha))
+    S_a = np.sin(np.radians(alpha))
+    CL_ml = Cz*C_a - Cx*S_a
+    CD_ml = Cx*C_a + Cz*S_a
 
     # Read into ParaView
     data_reader = pvs.LegacyVTKReader(registrationName=body_file.replace("dev/results/", ""), FileNames=body_file)
@@ -110,18 +109,17 @@ if __name__=="__main__":
     view.Update()
     display.XArrayName = 'Points_X'
     view.Update()
-    save_loc = 'dev/results/diamond_5_deg_full_{0}_data.csv'.format(grid)
-    pvs.SaveData(save_loc, proxy=plot, PointDataArrays=data_to_process, FieldAssociation='Point Data', Precision=12)
+    pvs.SaveData(data_file, proxy=plot, PointDataArrays=data_to_process, FieldAssociation='Point Data', Precision=12)
 
     # Read in data
-    data = np.genfromtxt(save_loc, delimiter=',', skip_header=1)
+    data = np.genfromtxt(data_file, delimiter=',', skip_header=1)
     
     # Plot data from MachLine
     plt.figure()
-    plt.plot(data[:,4], data[:,0], 'ks', label='2nd')
-    plt.plot(data[:,4], data[:,1], 'kv', label='Ise.')
-    plt.plot(data[:,4], data[:,2], 'ko', label='Slnd.')
-    plt.plot(data[:,4], data[:,3], 'k^', label='Lin.')
+    plt.plot(data[:,4], data[:,0], 'ks', markersize=3, label='2nd')
+    plt.plot(data[:,4], data[:,1], 'kv', markersize=3, label='Ise.')
+    plt.plot(data[:,4], data[:,2], 'ko', markersize=3, label='Slnd.')
+    plt.plot(data[:,4], data[:,3], 'k^', markersize=3, label='Lin.')
 
     # Plot data from shock-expansion theory
     x = np.linspace(0.0, 1.0, 100)
@@ -138,4 +136,41 @@ if __name__=="__main__":
     plt.xlabel('$x$')
     plt.ylabel('$C_p$')
     plt.legend()
-    plt.show()
+    plt.savefig(plot_dir+case_name+".pdf".format(M, alpha, int(half_angle)))
+    plt.close()
+
+    return CL_se, CD_se, CL_ml, CD_ml
+
+
+if __name__=="__main__":
+
+    grids = ["coarse", "medium", "fine", "ultra_fine"]
+    N_verts = [402, 1090, 3770, 13266]
+    Ms = [1.5, 2.0, 3.0, 5.0]
+    alphas = np.linspace(0.0, 5.0, 6)
+    half_angles = [5]
+
+    CLs = np.zeros((len(grids), len(Ms), len(alphas), len(half_angles)))
+    CDs = np.zeros((len(grids), len(Ms), len(alphas), len(half_angles)))
+
+    for i, grid in enumerate(grids):
+        for j, M in enumerate(Ms):
+            for k, alpha in enumerate(alphas):
+                for l, half_angle in enumerate(half_angles):
+
+                    _,_,CLs[i,j,k,l], CDs[i,j,k,l] = run_comparison(M, alpha, grid, half_angle)
+
+    for j, M in enumerate(Ms):
+        for k, alpha in enumerate(alphas):
+            for l,half_angle in enumerate(half_angles):
+
+                case_name = "M_{0}_aoa_{1}_{2}_deg_convergence".format(M, alpha, int(half_angle))
+
+                plt.figure()
+                plt.plot(N_verts, abs(CLs[:-1,j,k,l]-CLs[-1,j,k,l]), 'k-')
+                plt.xscale('log')
+                plt.yscale('log')
+                plt.xlabel('$N_\\text{verts}$')
+                plt.ylabel('$\\Delta C_L$')
+                plt.save(case_name+".pdf")
+                plt.close()
