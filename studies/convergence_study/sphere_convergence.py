1f0c3db77174d4e7f01a4d33074c1c2bfece56bc
Cory <cory.goates@aggiemail.usu.edu>
2023-01-18 12:37:31 -0700

    Updated sphere convergence study and gitignore.

--- studies/convergence_study/sphere_convergence.py
+++ studies/convergence_study/sphere_convergence.py
@@ -26,87 +26,82 @@ if __name__=="__main__":
 
     # Options
     densities = ["ultra_coarse", "very_coarse", "coarse", "medium"]
+    phis = np.radians([30.0, 45.0, 60.0])
+    thetas = np.radians([30.0, 45.0, 60.0])
     N = []
-    Cx_lower = []
-    Cy_lower = []
-    Cz_lower = []
-    Cx_higher = []
-    Cy_higher = []
-    Cz_higher = []
+    Cx = np.zeros((len(phis), len(thetas), len(densities)))
+    Cy = np.zeros((len(phis), len(thetas), len(densities)))
+    Cz = np.zeros((len(phis), len(thetas), len(densities)))
 
-    for i, density in enumerate(densities):
+    for i, phi in enumerate(phis):
+        for j, theta in enumerate(thetas):
+            for k, density in enumerate(densities):
 
-        # Initialize input
-        result_file = "studies/convergence_study/results/sphere_{0}.vtk".format(density)
-        report_file = "studies/convergence_study/reports/sphere_{0}.json".format(density)
+                # Initialize input
+                result_file = "studies/convergence_study/results/sphere_{0}.vtk".format(density)
+                report_file = "studies/convergence_study/reports/sphere_{0}.json".format(density)
 
-        # Lower-order results
-        input_dict ={
-            "flow" : {
-                "freestream_velocity" : [1.0, 1.0, 1.3]
-            },
-            "geometry" : {
-                "file" : "studies/convergence_study/meshes/sphere_{0}.stl".format(density),
-                "spanwise_axis" : "+y"
-            },
-            "solver" : {
-            },
-            "post_processing" : {
-            },
-            "output" : {
-                "body_file" : result_file,
-                "report_file" : report_file
-            }
-        }
+                # Lower-order results
+                input_dict ={
+                    "flow" : {
+                        "freestream_velocity" : [np.cos(phi)*np.cos(theta), np.sin(phi)*np.cos(theta), np.sin(theta)]
+                    },
+                    "geometry" : {
+                        "file" : "studies/convergence_study/meshes/sphere_{0}.stl".format(density),
+                        "spanwise_axis" : "+y"
+                    },
+                    "solver" : {
+                    },
+                    "post_processing" : {
+                    },
+                    "output" : {
+                        "body_file" : result_file,
+                        "report_file" : report_file
+                    }
+                }
 
-        # Run
-        report = run_machline(input_dict, "studies/convergence_study/input.json")
+                # Run
+                report = run_machline(input_dict, "studies/convergence_study/input.json")
 
-        # Store data
-        N.append(report["solver_results"]["system_dimension"])
-        Cx_lower.append(report["total_forces"]["Cx"])
-        Cy_lower.append(report["total_forces"]["Cy"])
-        Cz_lower.append(report["total_forces"]["Cz"])
+                # Store data
+                if i == 0 and j == 0:
+                    N.append(report["solver_results"]["system_dimension"])
+                Cx[i,j,k] = report["total_forces"]["Cx"]
+                Cy[i,j,k] = report["total_forces"]["Cy"]
+                Cz[i,j,k] = report["total_forces"]["Cz"]
 
-        # Higher-order results
-        input_dict ={
-            "flow" : {
-                "freestream_velocity" : [1.0, 1.0, 1.3]
-            },
-            "geometry" : {
-                "file" : "studies/convergence_study/meshes/sphere_{0}.stl".format(density),
-                "singularity_order" : "higher",
-                "spanwise_axis" : "+y"
-            },
-            "solver" : {
-            },
-            "post_processing" : {
-            },
-            "output" : {
-                "body_file" : result_file,
-                "report_file" : report_file
-            }
-        }
+    # Calculate convergence rates
+    orders = []
+    for i in range(len(phis)):
+        for j in range(len(thetas)):
 
-        # Run
-        report = run_machline(input_dict, "studies/convergence_study/input.json")
+            # Cx
+            err = abs(Cx[i,j,:])
+            coefs = np.polyfit(np.log(N), np.log(err), deg=1)
+            orders.append(coefs[0])
 
-        # Store data
-        Cx_higher.append(report["total_forces"]["Cx"])
-        Cy_higher.append(report["total_forces"]["Cy"])
-        Cz_higher.append(report["total_forces"]["Cz"])
+            # Cy
+            err = abs(Cy[i,j,:])
+            coefs = np.polyfit(np.log(N), np.log(err), deg=1)
+            orders.append(coefs[0])
+
+            # Cz
+            err = abs(Cz[i,j,:])
+            coefs = np.polyfit(np.log(N), np.log(err), deg=1)
+            orders.append(coefs[0])
+
+    avg_order = np.average(orders)
+    print("Average order of convergence: ", avg_order)
 
     # plot
     plt.figure()
-    plt.plot(N, np.abs(Cx_lower), 'k-', label='Lower-Order')
-    plt.plot(N, np.abs(Cy_lower), 'k-')
-    plt.plot(N, np.abs(Cz_lower), 'k-')
-    plt.plot(N, np.abs(Cx_higher), 'k--', label='Higher-Order')
-    plt.plot(N, np.abs(Cy_higher), 'k--')
-    plt.plot(N, np.abs(Cz_higher), 'k--')
+    for i in range(len(phis)):
+        for j in range(len(thetas)):
+            plt.plot(N, np.abs(Cx[i,j,:]), 'k-')
+            plt.plot(N, np.abs(Cy[i,j,:]), 'k-')
+            plt.plot(N, np.abs(Cz[i,j,:]), 'k-')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('$N_{verts}$')
     plt.ylabel('$C_F$')
-    plt.legend()
     plt.show()
