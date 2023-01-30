cf98bd913364289994e67aed2f9353b9dab94203
Cory <cory.goates@aggiemail.usu.edu>
2023-01-30 11:54:33 -0700

    Revamped delta wing comparison.

--- studies/delta_wing/delta_wing_comparison.py
+++ studies/delta_wing/delta_wing_comparison.py
@@ -8,61 +8,55 @@ from os import listdir, chdir, getcwd
 import os
 
 
-def data_plot(pressure_rule, angles_of_attack, semispan_locations):
+def plot_pressure_slices(pressure_rule, angles_of_attack, semispan_locations):
 
-    # Reformat computational method
+    # Get label for pressure rule
     if pressure_rule == "isentropic":
-        pressure_rule_code = "C_p_ise"
+        pressure_rule_label = "C_p_ise"
     elif pressure_rule == "slender-body":
-        pressure_rule_code = "C_p_sln"
+        pressure_rule_label = "C_p_sln"
     elif pressure_rule == "linear":
-        pressure_rule_code = "C_p_lin"
+        pressure_rule_label = "C_p_lin"
     elif pressure_rule == "second order":
-        pressure_rule_code = "C_p_2nd"
-
+        pressure_rule_label = "C_p_2nd"
     
     # Loop over angles of attack
-    for j,AoA in enumerate(angles_of_attack):
+    for j, AoA in enumerate(angles_of_attack):
 
         # Iterate over percent semispan locations
         for k, semi in enumerate(semispan_locations):
 
-            # pull in experimental data
-            exper_data_loc = "studies/delta_wing/experimental_data/delta_wing_exp_{0}_deg_aoa_{1}_semispan.csv".format(AoA,semi)
-            #experimental_data = np.genfromtxt(exper_data_loc, delimiter=",", dtype=float) # x in column 0, Cp in column 1 from Love, page 57
+            # Pull in experimental data
+            data_file_exp = "studies/delta_wing/experimental_data/delta_wing_exp_{0}_deg_aoa_{1}_semispan.csv".format(AoA,semi)
 
-            # Initialize figure and shapes to be plotted
-            plt.figure()
-            shape = ["o", "^", "s", "v"]
+            # Get MachLine data
+            data_file_ml = 'studies/delta_wing/results/delta_wing_{0}_semispan_{1}_deg_results.csv'.format(semi, AoA)
+            data_ml = np.genfromtxt(data_file_ml, delimiter=",", dtype=str)
 
-            data_location = 'studies/delta_wing/results/delta_wing_{0}_semispan_{1}_deg_results.csv'.format(semi, AoA)
-
-            # Identify which column of data is associated with selected comp method
-            column_data = np.genfromtxt(data_location, delimiter=",", dtype=str)
-            column_data = np.char.replace(column_data[0,:],'"',"")
-
-            # Identify locations of method data selected by user
+            # Identify which column of data is associated with selected pressure rule
+            data_ml = np.char.replace(data_ml[0,:], '"', "")
             Cp_loc = -1
             x_loc = -1
-            for i,col in enumerate(column_data):
-                if col == pressure_rule_code:
+            for i,col in enumerate(data_ml):
+                if col == pressure_rule_label:
                     Cp_loc = i
                 if col == "centroid:0":
                     x_loc = i
+
                 # Break out of loop if all points have been located
-                if min(Cp_loc,x_loc) > -1:
+                if min(Cp_loc, x_loc) > -1:
                     break
 
             # Read in data without column headers
-            data = np.genfromtxt(data_location, delimiter=",", skip_header=1, dtype=float)
-            # Format angle of attack for legend
-            aoa_formatted = str(AoA) + r"$^{\circ}$ deg"
+            data = np.genfromtxt(data_file_ml, delimiter=",", skip_header=1, dtype=float)
 
-            # Plot data
-            plt.plot(data[:,x_loc]/max(data[:,x_loc]), data[:,Cp_loc], color='k', label = 'MachLine',)
+            # Plot data from MachLine
+            plt.figure()
+            #plt.plot(data[:,x_loc]/max(data[:,x_loc]), data[:,Cp_loc], color='k', label = 'MachLine',)
+
             # plt.plot(data[:,x_loc]/max(data[:,x_loc]), data[:,Cp_loc],color='k', label = 'MachLine', marker = "s", linestyle="none")
             # plt.plot(experimental_data[:,0], experimental_data[:,1], color='k', marker=".", label="Experimental", linestyle="none", fillstyle="full")
-            #plt.plot(experimental_data[:,0], experimental_data[:,1], color='k', label="Experimental", marker = "o", linestyle="none", fillstyle='full')
+
             # pull in experimental data based on Angle of Attack
             if AoA == 0.0:
                 surf = ['']
@@ -73,16 +67,16 @@ def data_plot(pressure_rule, angles_of_attack, semispan_locations):
             
             # Initialize list to store marker shapes
             shape = ['o', 's']
+            fill = ['k', 'w']
 
-            
             # Iterate over upper and lower surface results from experimental data and plot each surface as a different marker
-            for i,surface in enumerate(surf):
-                surf_label = "Experimental " + surface[1:]
+            for i, surface in enumerate(surf):
+                surf_label = "Experimental " + surface[1:].title()
                 
-                exper_data_loc = "studies/delta_wing/experimental_data/delta_wing_exp_{0}_deg_aoa_{1}_semispan{2}.csv".format(AoA,semi,surface)
-                experimental_data = np.genfromtxt(exper_data_loc, delimiter=",", dtype=float) # x in column 0, Cp in column 1 from Love, page 57
+                data_file_exp = "studies/delta_wing/experimental_data/delta_wing_exp_{0}_deg_aoa_{1}_semispan{2}.csv".format(AoA,semi,surface)
+                experimental_data = np.genfromtxt(data_file_exp, delimiter=",", dtype=float) # x in column 0, Cp in column 1 from Love, page 57
                 
-                plt.plot(experimental_data[:,0], experimental_data[:,1], color='k', label=surf_label, marker = shape[i], linestyle="none", fillstyle='full')
+                plt.plot(experimental_data[:,0], experimental_data[:,1], 'ks', label=surf_label, mfc=fill[i])
                 
             # Plot theoretical data at 0 degrees AoA as presented by Love's work
             if AoA == 0.0:
@@ -90,35 +84,29 @@ def data_plot(pressure_rule, angles_of_attack, semispan_locations):
                 theory_loc = "studies/delta_wing/experimental_data/delta_wing_exp_theory_0.0_deg_aoa_{0}_semispan.csv".format(semi)
                 theory_data = np.genfromtxt(theory_loc, delimiter=",", dtype=float)
 
-                plt.plot(theory_data[:,0], theory_data[:,1], label="Theory", linewidth=1.5,color='k')
+                plt.plot(theory_data[:,0], theory_data[:,1], 'k:', label="Theory")
             
             # Differentiate between the upper and lower surfaces of MachLine's results
             half = round(len(data[:,x_loc])/2)
 
-            # breakpoint()
             # Plot upper surface
-            plt.plot(data[0:half,x_loc]/max(data[:,x_loc]), data[:half,Cp_loc],color='k',linestyle='--',label = 'MachLine upper',marker = '.',markersize=5,fillstyle='full')
+            plt.plot(data[0:half,x_loc]/max(data[:,x_loc]), data[:half,Cp_loc], 'k-', label='MachLine Upper')
             
             # Plot lower surface
-            plt.plot(data[half::,x_loc]/max(data[:,x_loc]),data[half:,Cp_loc],color='k',linestyle='--',label = 'MachLine lower',marker = '^', markersize=4,fillstyle='full')
-            # plt.plot(data[:,x_loc]/max(data[:,x_loc]), data[:,Cp_loc],color='k', label = 'MachLine', marker = "s", linestyle="none")
+            plt.plot(data[half::,x_loc]/max(data[:,x_loc]), data[half:,Cp_loc], 'k--', label = 'MachLine Lower')
 
             # Format plot
-            plt.xlabel("$\\frac{x}{l}$")
+            plt.xlabel("$\\frac{x}{c_r}$")
             plt.ylabel("$C_P$")
-            plt.xlabel(r"$\frac{x}{c_{root}}$")
-            y_title = r"$C_p$"
-            plt.ylabel(y_title)
             plt.gca().invert_yaxis()
             plt.legend()
-            # plt.title(f"{semi} percent semispan at {aoa_formatted}")
 
             # Save figure
             if not os.path.exists('studies/delta_wing/plots/'):
                 os.makedirs('studies/delta_wing/plots/')
             plot_loc = 'studies/delta_wing/plots/delta_wing_comparison_{0}_semispan_{1}_aoa_{2}.pdf'.format(semi, AoA, pressure_rule)
             plt.savefig(plot_loc)
-            #plt.show()
+            plt.close()
 
 
 def plot_force_convergence_over_AoA(AoA_list, M, densities):
@@ -202,7 +190,7 @@ def plot_force_convergence_over_AoA(AoA_list, M, densities):
     plt.ylabel('CD')
     plot_loc = 'studies/delta_wing/plots/delta_wing_CD_convergence.pdf'
     plt.savefig(plot_loc)
-    plt.show()
+    plt.close()
 
     # CL plot
     plt.figure()
@@ -216,8 +204,7 @@ def plot_force_convergence_over_AoA(AoA_list, M, densities):
     plt.ylabel('CL')
     plot_loc = 'studies/delta_wing/plots/delta_wing_CL_convergence.pdf'
     plt.savefig(plot_loc)
-    plt.show()
-
+    plt.close()
 
     # Print CD and CL results along with exerimental data, allowing for quanitification
     # of difference between MachLine and experimental resutls for paper purposes.
@@ -273,7 +260,6 @@ def plot_force_AoA(AoA_list, M):
         CD_fine.append(loc['Cx'] * np.cos(AoA*np.pi/180) + loc['Cy'] * np.sin(AoA*np.pi/180))
         CL_fine.append(-loc['Cx'] * np.sin(AoA*np.pi/180) + loc['Cy'] * np.cos(AoA*np.pi/180))
     
-    
     # Plot results against experimental data
 
     # CD plot
@@ -288,7 +274,7 @@ def plot_force_AoA(AoA_list, M):
     plt.ylim(bottom=0.0)
     plot_loc = 'studies/delta_wing/plots/delta_wing_CD_comparison.pdf'
     plt.savefig(plot_loc)
-    plt.show()
+    plt.close()
 
     # CL plot
     plt.figure()
@@ -300,7 +286,7 @@ def plot_force_AoA(AoA_list, M):
     plt.ylabel('$C_L$')
     plot_loc = 'studies/delta_wing/plots/delta_wing_CL_comparison.pdf'
     plt.savefig(plot_loc)
-    plt.show()
+    plt.close()
 
     # Print CD and CL results along with exerimental data, allowing for quanitification
     # of difference between MachLine and experimental resutls for paper purposes.
@@ -317,7 +303,7 @@ def plot_force_AoA(AoA_list, M):
     # file = np.savetxt(csv_loc, force_array, delimiter=',')
 
 
-if __name__=="__main__":
+def run_pressure_distribution_comparison(run_machline=False):
 
     # Parameters
     M = 1.62
@@ -325,23 +311,88 @@ if __name__=="__main__":
     gamma = 1.4
     T_inf = 300.0
     c_inf = np.sqrt(gamma*R_G*T_inf)
-    # angles_of_attack = [0.0, 2.0, 4.1, 8.5, 10.75]
-    angles_of_attack = [-6., -5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5.]
-    # angles_of_attack = [0.0]
+    angles_of_attack = [0.0, 2.0, 4.1, 8.5, 10.75]
     semispan_loc = [22.5, 64.1]
-    b_half = (0.463/2/.230) # semispan length nondimensionalized by root chord
-    R_A = 4.023
-    # b_half = 0.2315 # semispan length for OpenVSP model
+    b_half = (0.463/2/.230)
     mesh_type = 'clustered'
     mesh_density = 'fine' # coarse, semi_fine, fine are the options created with 20, 40, and 80 nodes respectively
     mesh_filetype = 'vtk'
-    force_plot_densities = ['coarse', 'semi_fine', 'fine']
 
-    # Check working directory and re-route if necessary
-    check_dir = getcwd()
+    # Run MachLine
+    if run_machline:
+        run_machline_cases(angles_of_attack, mesh_type, M, c_inf, mesh_density, mesh_filetype, gamma, b_half)
 
-    if 'delta_wing' in check_dir:
-        chdir("../../")
+    for alpha in angles_of_attack:
+
+        # Read into ParaView
+        body_file = "studies/delta_wing/results/delta_wing_{0}_deg_{1}.vtk".format(alpha, mesh_density)
+        data_reader = pvs.LegacyVTKReader(registrationName=body_file.replace("studies/delta_wing/results", ""), FileNames=body_file)
+
+        # Filter cell data to point data
+        filter = pvs.CellDatatoPointData(registrationName='Filter', Input=data_reader)
+        data_to_process = ['C_p_ise', 'C_p_2nd', 'C_p_lin', 'C_p_sln', 'centroid']
+        filter.CellDataArraytoprocess = data_to_process
+        fields_to_process = 'Point Data'
+
+        # Iterate over semispan locations
+        for i, percent_semispan in enumerate(semispan_loc):
+            semispan_str = str(percent_semispan)
+            percent_semi = percent_semispan * b_half / 100.0
+
+            # Slice mesh at each semispan location
+            slicer = pvs.PlotOnIntersectionCurves(registrationName="Slice", Input=filter)
+            slicer.SliceType = 'Plane'
+            
+            if 'VSP' in mesh_type:
+                origin = [0.0, percent_semi, 0.0]
+                normal = [0.0, 1.0, 0.0]
+            else:
+                origin = [0.0, 0.0, percent_semi]
+                normal = [0.0, 0.0, 1.0]
+
+            slicer.SliceType.Origin = origin
+            slicer.SliceType.Normal = normal
+
+            # Extract and save data
+            plot = pvs.PlotData(registrationName="Plot")
+            view = pvs.CreateView('XYChartView')
+            display = pvs.Show(plot, view, 'XYChartRepresentation')
+            display.XArrayName = 'centroid_X'
+            view.Update()
+            save_loc = 'studies/delta_wing/results/delta_wing_{0}_semispan_{1}_deg_results.csv'.format(semispan_str, alpha)
+            pvs.SaveData(save_loc, proxy=plot, CellDataArrays=data_to_process, FieldAssociation='Point Data')
+
+    # Plot pressure rule method over a range of angles of attack at each semispan location
+    pressure_rule = "isentropic" # isentropic, second order, slender-body, or linear
+    plot_pressure_slices(pressure_rule, angles_of_attack, semispan_loc)
+
+
+def run_force_comparison(run_machline=False):
+
+    # Parameters
+    M = 1.62
+    R_G = 287.058
+    gamma = 1.4
+    T_inf = 300.0
+    c_inf = np.sqrt(gamma*R_G*T_inf)
+    angles_of_attack = [-6., -5., -4., -3., -2., -1., 0., 1., 2., 3., 4., 5.]
+    semispan_loc = [22.5, 64.1]
+    b_half = (0.463/2/.230)
+    mesh_type = 'clustered'
+    mesh_density = 'fine' # coarse, semi_fine, fine are the options created with 20, 40, and 80 nodes respectively
+    mesh_filetype = 'vtk'
+    convergence_plot_densities = ['coarse', 'semi_fine', 'fine']
+
+    # Run MachLine
+    if run_machline:
+        run_machline_cases(angles_of_attack, mesh_type, M, c_inf, mesh_density, mesh_filetype, gamma, b_half)
+
+    # Plot
+    plot_force_AoA(angles_of_attack, M)
+    plot_force_convergence_over_AoA(angles_of_attack, M, convergence_plot_densities)
+
+
+def run_machline_cases(angles_of_attack, mesh_type, M, c_inf, mesh_density, mesh_filetype, gamma, b_half):
 
     # Iterate over angles of attack
     for alpha in angles_of_attack:
@@ -351,7 +402,7 @@ if __name__=="__main__":
             freestream = [M*c_inf*np.cos(np.radians(alpha)), M*c_inf*np.sin(np.radians(alpha)), 0.0]
         
         # Declare MachLine input
-        body_file = "studies/delta_wing/results/delta_wing_{0}_deg_{1}.vtk".format(alpha,mesh_density)
+        body_file = "studies/delta_wing/results/delta_wing_{0}_deg_{1}.vtk".format(alpha, mesh_density)
         input_dict = {
             "flow": {
                 "freestream_velocity": freestream,
@@ -366,7 +417,7 @@ if __name__=="__main__":
                     "trefftz_distance": 20,
                 },
                 "reference": {
-                    "area": (b_half) * 1 # 1 represents the root chord for this wing. b_half is the semispan
+                    "area": b_half*1.0 # 1 represents the root chord for this wing. b_half is the semispan
                 }
             },
             "solver": {
@@ -398,55 +449,23 @@ if __name__=="__main__":
             json.dump(input_dict, input_handle, indent=4)
 
         # Run
-        #sp.run(["./machline.exe", input_file])
+        sp.run(["./machline.exe", input_file])
         
         # Verify that MachLine execution was successful
-        control_point_file = "delta_wing_{0}_deg_{1}_control_points.vtk".format(alpha,mesh_density)
+        control_point_file = "delta_wing_{0}_deg_{1}_control_points.vtk".format(alpha, mesh_density)
         if control_point_file not in listdir("studies/delta_wing/results/"):
             print(f"The desired file cannot be located:  studies/delta_wing/results/{control_point_file}")
             exit()
 
-        # Read into ParaView
-        data_reader = pvs.LegacyVTKReader(registrationName=body_file.replace("studies/delta_wing/results", ""), FileNames=body_file)
 
-        # Filter cell data to point data
-        filter = pvs.CellDatatoPointData(registrationName='Filter', Input=data_reader)
-        data_to_process = ['C_p_ise', 'C_p_2nd', 'C_p_lin', 'C_p_sln', 'centroid']
-        filter.CellDataArraytoprocess = data_to_process
-        fields_to_process = 'Point Data'
+if __name__=="__main__":
 
-        # Iterate over semispan locations
-        for i, percent_semispan in enumerate(semispan_loc):
-            semispan_str = str(percent_semispan)
-            percent_semi = percent_semispan * b_half / 100.0
+    # Check working directory and re-route if necessary
+    check_dir = getcwd()
 
-            # Slice mesh at each semispan location
-            slicer = pvs.PlotOnIntersectionCurves(registrationName= "Slice", Input=filter)
-            slicer.SliceType = 'Plane'
-            # slicer.HyperTreeGridSlicer = 'Plane'
-            
-            if 'VSP' in mesh_type:
-                origin = [0.0, percent_semi, 0.0]
-                normal = [0.0, 1.0, 0.0]
-            else:
-                origin = [0.0, 0.0, percent_semi]
-                normal = [0.0, 0.0, 1.0]
+    if 'delta_wing' in check_dir:
+        chdir("../../")
 
-            slicer.SliceType.Origin = origin
-            slicer.SliceType.Normal = normal
-
-            # Extract and save data
-            plot = pvs.PlotData(registrationName="Plot")
-            view = pvs.CreateView('XYChartView')
-            display = pvs.Show(plot, view, 'XYChartRepresentation')
-            display.XArrayName = 'centroid_X'
-            view.Update()
-            save_loc = 'studies/delta_wing/results/delta_wing_{0}_semispan_{1}_deg_results.csv'.format(semispan_str, alpha)
-            pvs.SaveData(save_loc, proxy=plot, ChooseArraysToWrite=1, CellDataArrays=data_to_process, FieldAssociation='Cell Data')
-
-    # Plot pressure rule method over a range of angles of attack at each semispan location
-    #pressure_rule = "isentropic" # isentropic, second order, slender-body, or linear
-    #data_plot(pressure_rule, angles_of_attack, semispan_loc)
-
-    # Plot CL CD CM
-    plot_force_AoA(angles_of_attack, M)
+    # Run comparisons
+    run_pressure_distribution_comparison(run_machline=False)
+    run_force_comparison(run_machline=False)
