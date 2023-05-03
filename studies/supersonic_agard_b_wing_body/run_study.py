20c7c85082fcdb2433f70ae63bc34d638ec21e14
Cory <cory.goates@aggiemail.usu.edu>
2023-05-03 12:35:25 -0600

    Working on AGARD-B study.

--- studies/supersonic_agard_b_wing_body/run_study.py
+++ studies/supersonic_agard_b_wing_body/run_study.py
@@ -1,6 +1,80 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from studies.case_running_functions import run_quad, write_input_file, cases, line_styles
+
+RERUN_MACHLINE = False
+study_dir = "studies/supersonic/agard_b_wing_body/"
+plot_dir = study_dir + "plots/"
+
+
+def run_quad_for_mach_and_mesh(M, grid):
+    """Runs a case quad for the given Mach number and mesh density."""
+
+    # Storage locations
+    case_name = "M_{0}_{1}".format(M, grid)
+    mesh_file = study_dir + "meshes/diamond_{0}_deg_full_{1}.stl".format(int(half_angle), grid)
+    results_file = study_dir + "results/"+case_name+".vtk"
+    report_file = study_dir + "reports/"+case_name+".json"
+
+    # Write out input file
+
+    # Declare MachLine input
+    input_dict = {
+        "flow": {
+            "freestream_velocity": [M*c_inf*np.cos(np.radians(alpha)), 0.0, M*c_inf*np.sin(np.radians(alpha))],
+            "gamma" : gamma,
+            "freestream_mach_number" : M
+        },
+        "geometry": {
+            "file": mesh_file,
+            "spanwise_axis" : "+y",
+            "max_continuity_angle" : MCA,
+            "wake_model": {
+                "append_wake" : False,
+            },
+            "reference": {
+                "area": 4.0
+            }
+        },
+        "solver": {
+            "formulation": "morino"
+        },
+        "post_processing" : {
+            "pressure_rules" : {
+                "second-order" : True,
+                "isentropic" : True,
+                "slender-body" : True,
+                "linear" : True
+            }
+        },
+        "output" : {
+            "body_file" : results_file,
+            "report_file" : report_file
+        }
+    }
+
+    # Dump
+    input_file = study_dir + "input.json"
+    write_input_file(input_dict, input_file)
+
+    # Run quad
+    reports = run_quad(input_file, run=RERUN_MACHLINE)
+
+    # Pull out forces
+    C_F = np.zeros((4,3))
+    for i, report in enumerate(reports):
+        C_F[i,0] = report["total_forces"]["Cx"]
+        C_F[i,1] = report["total_forces"]["Cy"]
+        C_F[i,2] = report["total_forces"]["Cz"]
+
+    # Get system dimension and average characteristic length
+    N_sys = reports[0]["solver_results"]["system_dimension"]
+    l_avg = reports[0]["mesh_info"]["average_characteristic_length"]
+
+    return N_sys, l_avg, C_F
+
+
 
 if __name__=="__main__":
 
