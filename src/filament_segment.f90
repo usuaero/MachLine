506a09dfd6dd6fb0a887224099c4689049c692c0
Spencer-Adams <spencer.j.adams22@gmail.com>
2023-09-08 08:07:55 -0600

    some vel influence stuff

--- src/filament_segment.f90
+++ src/filament_segment.f90
@@ -2,64 +2,71 @@ module filament_segment_mod
     use base_geom_mod
     use helpers_mod
     use mesh_mod 
+    use math_mod 
 
 !!!! mirrors panel module
 
     implicit none
 
 
-    type filament_dod
-        ! Container type for parameters of whether a filament lies in a point's domain of dependence
-
-        logical :: in_dod = .true.
-        logical,dimension(3) :: edges_in_dod = .true.
-
-    end type filament_dod
+    type filament_segment_integrals 
+        ! Container type for the fundamental integrals used to calculate influence coefficients
+        
+        integer :: r, s, rs ! Parameters that will be most convenient to keep here (flow type indicators)
+        real :: u, v, w ! unrelaxed influence integrals in the compressible frame. !!!! may need them to be allocatable
 
 
-    type :: filament_segment !!!! changed to filament_segment type
+    end type filament_segment_integrals
 
-            type(vertex_pointer), dimension(:),allocatable :: vertices 
-            integer :: index ! index of this filament in the filament mesh array
-        integer :: N = 2 ! number of vertices
-
-        contains
-            ! initilizers 
-            procedure :: init => filament_segment_init
-        integer :: N = 2 ! Number of sides/vertices 
-
-        contains
-            procedure :: init => filament_segment_init 
-            procedure :: calc_velocity_influences => filament_segment_calc_velocity_influences
-            procedure :: check_dod => filament_check_dod
-            procedure :: calc_integrals => filament_segment_calc_integrals
-            ! Getters
-            procedure :: get_vertex_index => filament_segment_get_vertex_index
-
-    end type filament_segment 
-
-            
 
     type filament_dod
     ! container for whether a filament lies in a point's domain of dependence
     
-        logical :: in_dod = .true.
+        logical :: first_in_dod = .true. !!!! the nomenclature can certainly change here
         logical,dimension(2) :: both_in_dod = .true. !!!! the nomenclature can certainly change here
+        logical,dimension(2) :: neither_in_dod = .true. !!!! the nomenclature can certainly change here
 
     end type filament_dod
 
 
+    type filament_segment 
+        ! A section of a filament that contains two vertices. 
+        type(vertex_pointer), dimension(:),allocatable :: vertices 
+        integer :: index ! index of this filament in the filament mesh array
+        integer :: N = 2 ! number of vertices
+        integer :: 
+
+        contains
+            procedure :: init => filament_segment_init 
+            procedure :: check_dod => filament_check_dod
+            procedure :: calc_integrals => filament_segment_calc_integrals
+            ! Getters
+            procedure :: get_vertex_index => filament_segment_get_vertex_index
+            !velocities
+            procedure :: calc_velocity_influences => filament_segment_calc_velocity_influences
+            procedure :: assemble_v_d_M_space => filament_segment_assemble_v_d_M_space 
+            procedure :: calc_influence_integrals => filament_segment_calc_influence_integrals 
+
+    end type filament_segment 
+
+
 contains
 
+
     subroutine filament_segment_init(this,v1,v2,index)
+        ! initializes important filament segment parameters 
+
         implicit none
 
         class(filament_segment), intent(inout) :: this
         type(vertex),intent(inout),target :: v1, v2
         integer, intent(in) :: index
 
+        ! set number of sides 
+        this%N = 2
+
         ! allocate vertex array
-        allocate(this%vertices(2))
+        allocate(this%vertices(this%N))
 
         ! assign pointers
         this%vertices(1)%ptr => v1
@@ -71,7 +78,9 @@ contains
         ! still need to calculated derived geometry (based on what is needed in solver)
     end subroutine filament_segment_init
 
+
     function filament_segment_get_vertex_index(this, i) result(index)
+        ! gets a vertex index
 
         implicit none
 
@@ -79,11 +88,15 @@ contains
         integer,intent(in) :: i
         integer :: index
         index = this%vertices(i)%ptr%index
+
     end function filament_segment_get_vertex_index
 
 
     subroutine filament_segment_calc_velocity_influences(this, F, freestream, mirror_filament, v_d_M_space)
-       
+        ! calculates the velocity influence of a vortex filament 
+        
+        implicit none 
+
         class(filament_segment),intent(in) :: this
         real,dimension(3),intent(in) :: F !!!! not sure what P is in panel_calc_velocity_influences
         type(flow),intent(in) :: freestream
@@ -91,7 +104,8 @@ contains
         real, dimension(:,:),allocatable,intent(out) :: v_d_M_space
 
         type(filament_dod) :: dod_info !!!! might need to make a filament_dod type in this mod (filament_segment_mod)
-        type(filament_eval_point_geom) :: geom !!!! go to base_geom and make this type? Or come up with another way. 
+        type(eval_point_geom) :: geom !!!! go to base_geom and make this type? Or come up with another way. 
+        type(filament_segment_integrals) :: int 
         real,dimension(:,:),allocatable :: v_d_mu_space
         real :: x2, y2 !!!! not sure if this is needed
         integer :: i
@@ -105,7 +119,7 @@ contains
             end if 
              ! Get integrals 
             int = this%calc_integrals(geom, 'velocity', freestream, mirror_filament, dod_info)
-            v_d_M_space =  !!!!!
+            v_d_M_space =  this%assemble_v_d_M_space(int, geom, freestream, mirror_filament)
         end if 
 
     end subroutine filament_segment_calc_velocity_influences
@@ -113,6 +127,7 @@ contains
 
     function filament_segment_check_dod(this, eval_point, freestream, mirror_filament) result(dod_info)
         ! Determines how (if) this panel lies within the domain of dependence of the evaluation point
+        
         implicit none 
 
         class(filament_segment),intent(in) :: this
@@ -161,9 +176,9 @@ contains
                 dod_info%in_dod = .true.
                 dod_info%edges_in_dod = .true.
                 
-
-        end if
-        
+            end if 
+        end if !!!! use Josh's stuff here - SA 
+    end do 
         
 
     end function filament_segment_check_dod
@@ -172,35 +187,55 @@ contains
     function filament_segment_calc_integrals(this, geom, influence_type, freestream, mirror_filament, dod_info) result(int)
         ! Calculates the integrals necessary for the given influence
         
-        class(filamenet_segment),intent(in) :: this 
-        type(eval_point_geom),intent(in) :: geom
-        character(len=*),intent(in) :: influence_type
-        type(flow), intent(in) :: freestream
-        logical,intent(in) :: mirror_filament
-
-        type(integrals) :: int
-        type(filament_dod),intent(in) :: dod_info
-
-    end function filament_segment_calc_integrals 
-
-
-
-    end function filament_segment_check_dod
-
-
-    function filament_segment_calc_integrals(this, geom, influence_type, freestream, mirror_filament, dod_info) result(int)
-        ! Calculates the integrals necessary for the given influence
+        implicit none 
         
         class(filamenet_segment),intent(in) :: this 
         type(eval_point_geom),intent(in) :: geom
-        character(len=*),intent(in) :: influence_type
+        ! character(len=*),intent(in) :: influence_type !!!! don't think this will apply - SA 
         type(flow), intent(in) :: freestream
         logical,intent(in) :: mirror_filament
 
-        type(integrals) :: int
+        type(filament_segment_integrals) :: int
         type(filament_dod),intent(in) :: dod_info
 
+        int%s = freestream%s 
+        int%rs = int%r*int%s 
+
+        ! Calculate necessary integrals based on the flow condition
+        call this%calc_influence_integrals(geom, dod_info, freestream, mirror_filament, int) !!!! I think the int input in this subroutine call is what will output from this function
+        !!!! should dod_info carry whether the dod is supersonic or subsonic? -SA 
+
+ 
     end function filament_segment_calc_integrals 
+
+
+    subroutine filament_segment_calc_influence_integrals(this, geom, dod_info, freestream, mirror_filament, int)
+        ! calculates the supersonic and subsonic vortex filament influence integrals
+
+        implicit none 
+
+        class(filament_segment),intent(in) :: this
+        type(eval_point_geom),intent(in) :: geom 
+        type(dod),intent(in) :: dod_info
+        type(flow),intent(in) :: freestream 
+        logical,intent(in) :: mirror_filament
+        type(filament_segment_integrals),intent(inout) :: int
+        real :: unrel_v_numer_one, unrel_v_numer_two
+        real :: unrel_v_denom_one, unrel_v_denom_two
+
+        call this%get_vertex_location() !!!! put in appropriate input and get 
+        
+        ! write out the integrals here based on the different dod stuff. 
+
+       !if (this%relaxed) !!!! put in logic like this later when we employ relaxation. -SA  
+        if (dod_info%both_in_dod) then 
+            int%u = 0
+            int%v = (1/(2*pi*k))*((((zo-zf)*(xo-xf))/(()()))) !!!! declare all of these variables and stuff - SA
+        
+        end if 
+    
+    end subroutine filament_segment_calc_influence_integrals
+
 
 
 
