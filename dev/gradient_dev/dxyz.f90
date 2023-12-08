fc5d96354b20264d55ebeab34534618eb4e5cfa7
Nathan Hoch <nathan.hoch@usu.edu>
2023-12-08 15:52:39 -0700

    rearranged some dxyz stuff

--- dev/gradient_dev/dxyz.f90
+++ dev/gradient_dev/dxyz.f90
@@ -6,7 +6,7 @@ program dxyz
     
     implicit none
 
-    type(panel) :: panel_1, panel_2, panel_3, panel_4, panel_5, panel_6
+    type(panel),dimension(:),allocatable :: panels
     type(flow) :: freestream
     type(eval_point_geom) :: geom
     type(integrals) :: int
@@ -19,10 +19,27 @@ program dxyz
     type(panel),allocatable,dimension(:) :: panels    ! list of panels, this should be a mesh attribute
     character(len=:),allocatable :: spanwise_axis
 
+    
+    ! Initialize freestream
+    freestream%supersonic = .false.
+    freestream%v_inf = (/1., 0., 0./)
+    freestream%c_hat_g = (/1., 0., 0./) ! freestream direction vector
+    freestream%U = 1.                   ! freestream magnitude
+    freestream%U_inv = 1.
+    freestream%M_inf = 0.               ! incompressible, Mach = 0
+    freestream%B = 1.                   ! supersonic compressibility scaling factor
+    freestream%s = 1.                   ! flow type indicator (1 = subsonic)
+    freestream%K = 4.*pi                ! Kappa factor (4 pi) for subsonic, (2 pi) for supersonic
+    freestream%K_inv = 1./(4.*pi)       
+    spanwise_axis = 'y+'
+    call freestream%calc_metric_matrices()
+    call freestream%calc_transforms(spanwise_axis)
+
+
     ! For this simple example, we don't need to collapse dublicates
     N_verts = 6
     N_panels = 8
-
+    
     ! Initialize panels
     v1 = (/ 0.0, 0.0,-0.2/)  ! top
     v2 = (/ 1.0, 0.0, 0.0/)  ! forward 
@@ -53,61 +70,28 @@ program dxyz
 
     ! initialize 8 panels
     allocate(panels(N_panels))
-    call panels(i)%init(vertices(i1), vertices(i2), vertices(i3), i)
-    call panel_1%init(vert1, vert2, vert3, 1, .false.) !    top, right, forward
-    call panel_2%init(vert1, vert3, vert4, 2, .false.) !    top, right,     aft 
-    call panel_3%init(vert1, vert4, vert5, 3, .false.) !    top,  left,     aft
-    call panel_4%init(vert1, vert5, vert2, 4, .false.) !    top,  left, forward
-
-    call panel_5%init(vert6, vert2, vert3, 5, .false.) ! bottom, right, forward
-    call panel_6%init(vert6, vert3, vert4, 6, .false.) ! bottom, right,     aft
-    call panel_7%init(vert6, vert4, vert5, 7, .false.) ! bottom,  left,     aft
-    call panel_8%init(vert6, vert5, vert2, 8, .false.) ! bottom,  left, forward
-
-
-
-
-
     
-    ! Initialize points
-    points(:,1) = (/0., 0., 1./)
-    points(:,2) = (/0., 0., 2./)
-    points(:,3) = (/1., 1., 1./)
-    points(:,4) = (/2., 2., 2./)
-    points(:,5) = (/0., 0., -1./)
-    points(:,6) = (/0.5, 0.5, 0./)
-    
-    ! Initialize freestream
-    freestream%supersonic = .false.
-    freestream%v_inf = (/1., 0., 0./)
-    freestream%c_hat_g = (/1., 0., 0./) ! freestream direction vector
-    freestream%U = 1.                   ! freestream magnitude
-    freestream%U_inv = 1.
-    freestream%M_inf = 0.               ! incompressible, Mach = 0
-    freestream%B = 1.                   ! supersonic compressibility scaling factor
-    freestream%s = 1.                   ! flow type indicator (1 = subsonic)
-    freestream%K = 4.*pi                ! Kappa factor (4 pi) for subsonic, (2 pi) for supersonic
-    freestream%K_inv = 1./(4.*pi)       
-    spanwise_axis = 'y+'
-    call freestream%calc_metric_matrices()
-    call freestream%calc_transforms(spanwise_axis)
+    call panels(1)%init(vert1, vert2, vert3, 1, .false.) !    top, right, forward
+    call panels(2)%init(vert1, vert3, vert4, 2, .false.) !    top, right,     aft 
+    call panels(3)%init(vert1, vert4, vert5, 3, .false.) !    top,  left,     aft
+    call panels(4)%init(vert1, vert5, vert2, 4, .false.) !    top,  left, forward
+
+    call panels(5)%init(vert6, vert2, vert3, 5, .false.) ! bottom, right, forward
+    call panels(6)%init(vert6, vert3, vert4, 6, .false.) ! bottom, right,     aft
+    call panels(7)%init(vert6, vert4, vert5, 7, .false.) ! bottom,  left,     aft
+    call panels(8)%init(vert6, vert5, vert2, 8, .false.) ! bottom,  left, forward
+
+    ! panel init calculates the following:
+    !   normal vector
+    !   area
+    !   centroid
+    !   radius
+    !   n_hat_g (g edge vectors)
+
+    ! allocate a panel attribute dxyz
 
 
     ! Init with flow
-    call my_panel%init_with_flow(freestream, .false., 0)
+    !call my_panel%init_with_flow(freestream, .false., 0)
 
-    ! Loop through points
-    do i=1,6
-
-        ! Get geometry
-        geom = my_panel%calc_subsonic_geom(points(:,i), freestream, .false.)
-
-        ! Get integrals
-        allocate(int%F111(3), source=0.)
-        call my_panel%calc_subsonic_edge_integrals(geom, freestream, .false., int)
-        call my_panel%calc_subsonic_panel_integrals(geom, freestream, .false., int)
-        deallocate(int%F111)
-        write(*,*) int%hH113/(4.*pi)
-    end do
-    
 end program dxyz
