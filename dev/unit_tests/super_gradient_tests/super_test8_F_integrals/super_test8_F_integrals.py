447edcfe6fb34cb0d32d3a9edadb000c08ddb132
NATHAN HOCH LAPTOP <nathan.h.hoch@gmail.com>
2024-05-12 22:27:56 -0600

    working on super test 8

--- dev/unit_tests/super_gradient_tests/super_test8_F_integrals/super_test8_F_integrals.py
+++ dev/unit_tests/super_gradient_tests/super_test8_F_integrals/super_test8_F_integrals.py
@@ -14,7 +14,7 @@ if __name__=="__main__":
             "src/wake_strip.f90", "src/wake_mesh.f90", "src/surface_mesh.f90", 
             "src/panel_solver.f90", 
             
-            "dev/unit_tests/gradient_tests2/test8_F_integrals/test8_F_integrals.f90"])
+            "dev/unit_tests/super_gradient_tests/super_test8_F_integrals/super_test8_F_integrals.f90"])
 
     # Run
     sp.run(["a.exe"])
